048fac3fff32dd608a7d3d06a255d3ac610ee792
Tiara <tburtin1019@gmail.com>
2023-03-02 09:18:50 -0600

    saving lesson 2

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -24,9 +24,16 @@ export class CandidatesComponent implements OnInit {
   }
 
   // Code the addToCrew function here:
+addToCrew(candidate: object) {
+  if (!this.crew.includes(candidate)) {
+    this.crew.push(candidate)
+  }
+}
 
 
   // BONUS: Code the changeMissionName function here:
-
+changeMissionName(string: string) {
+  this.missionName = string 
+}
 
 }
